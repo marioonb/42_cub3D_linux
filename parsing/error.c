6002b5f307c2c02886f4654dd1de0791ba947b45
User42 <User42>
2020-09-17 22:32:10 +0200

    segfault moove gauche + erreurs

--- parsing/error.c
+++ parsing/error.c
@@ -22,7 +22,7 @@ void		ft_error_fd(int index, char *s)
 	else if (index == 3)
 		mini_printf("Too much element\n");
 	else if (index == 4)
-		mini_printf("%s missing or syntaxe error or no mapfile\n", s);
+		mini_printf("%s missing or syntax error or no mapfile\n", s);
 	if (index == 5)
 		mini_printf("error malloc - %s", s);
 	exit(EXIT_FAILURE);
@@ -34,11 +34,11 @@ void		ft_error_color_fd(int index, char c)
 	if (index == 1)
 		mini_printf("color code not numeric or wrong format for %c\n", c);
 	if (index == 2)
-		mini_printf("code color no entry for %c\n", c);
+		mini_printf("color code not entered for %c\n", c);
 	else if (index == 4)
-		mini_printf("error code color for %c - value between 0 and 255\n", c);
+		mini_printf("error color code for %c - value between 0 and 255\n", c);
 	else if (index == 5)
-		mini_printf("so much element for %c\n", c);
+		mini_printf("too many elements for %c\n", c);
 	exit(EXIT_FAILURE);
 }
 
@@ -46,24 +46,24 @@ void		ft_error_map(int index, char a, int b, int c)
 {
 	mini_printf("Error\n");
 	if (index == 1)
-		mini_printf("wall around map not define, position : %d, %d or jump \n",
+		mini_printf("wall around map undefined, position : %d, %d or jump \n",
 		b, c);
 	else if (index == 2)
-		mini_printf("map's wall West not define, line : %d\n", b + 1);
+		mini_printf("map's wall West undefined, line : %d\n", b + 1);
 	else if (index == 3)
-		mini_printf("map's east not define, line : %d\n", b + 1);
+		mini_printf("map's east undefined, line : %d\n", b + 1);
 	else if (index == 4)
-		mini_printf("map invalide element %c, line : %d, row :  %d\n",
+		mini_printf("invalid map element %c, line : %d, row :  %d\n",
 		a, b + 1, c);
 	else if (index == 5)
-		mini_printf("no wall around position %d, %d or caractere %c invalide\n",
+		mini_printf("no wall around position %d, %d or invalid character %c \n",
 			b, c, a);
 	else if (index == 6)
-		mini_printf("map not find or not valid\n");
+		mini_printf("map not found or invalid\n");
 	else if (index == 7)
-		mini_printf("full position player");
+		mini_printf("multiple position player");
 	else if (index == 8)
-		mini_printf("player position not define");
+		mini_printf("player position undefined");
 	exit(EXIT_FAILURE);
 }
 
@@ -71,7 +71,7 @@ void		ft_error_div(int index)
 {
 	mini_printf("Error\n");
 	if (index == 1)
-		mini_printf("texture not charged\n");
+		mini_printf("unloaded texture\n");
 	if (index == 2)
 		mini_printf("incorrect image format - enter 64X64\n");
 	if (index == 3)
@@ -87,11 +87,11 @@ void		ft_error_div(int index)
 	if (index == 8)
 		mini_printf("error new image no create\n");
 	if (index == 9)
-		mini_printf("so much element for resolution\n");
+		mini_printf("too much element for resolution\n");
 	if (index == 10)
 		mini_printf("please enter heigth resolution\n");
 	if (index == 11)
-		mini_printf("jump in map or so much jump after map\n");
+		mini_printf("jump in map or too much jump after map\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -99,10 +99,10 @@ void		ft_error_texture(int index, char *s)
 {
 	mini_printf("Error\n");
 	if (index == 1)
-		mini_printf("texture %s not charged or error path\n", s);
+		mini_printf("unloaded texture %s or error path\n", s);
 	if (index == 2)
 		mini_printf("%s is not a correct texture\n", s);
 	if (index == 3)
-		mini_printf("so much element for %s\n", s);
+		mini_printf("too much element for %s\n", s);
 	exit(EXIT_FAILURE);
 }
